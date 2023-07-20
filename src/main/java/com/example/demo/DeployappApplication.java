aba64c0d1767f8265042c1b770ff914bef360390
Pratiksha Gurram <47708982+pratikshagurram@users.noreply.github.com>
2023-07-20 11:15:00 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/DeployappApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DeployappApplication {
+@GetMapping("/")
+public String getMessage()
+{
+	return "Hello Guys!! How are you?";
+}
+@PostMapping("/{name}")
+public String getMessage(@RequestBody String name)
+{
+	return "I am "+name;
+}
+	public static void main(String[] args) {
+		SpringApplication.run(DeployappApplication.class, args);
+	}
+
+}
